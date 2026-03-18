b88b6d99aa4a27c6ae6a0429c20c3a9806f4a060
DhidelOsorio <djosorio@ufm.edu>
2026-03-18 10:52:08 -0600

    Se hicieron los cambios principalmente en la clase Node

--- Linked_list/ll.py
+++ Linked_list/ll.py
@@ -1,19 +1,25 @@
 from __future__ import annotations
-from typing import Any, Iterable, Iterator, List, Optional
+from typing import Any, Dict, Iterable, Iterator, List, Optional
 
 
 class Node:
-    def __init__(self, data: Any) -> None:
-        self.data: Any = data
+    def __init__(self, name: str, artist: str, album: str) -> None:
+        self.data: Dict[str, str] = {
+            "name": name,
+            "artist": artist,
+            "album": album
+        }
         self.next: Optional[Node] = None
+        self.prev: Optional[Node] = None
 
     def __repr__(self) -> str:
-        return f"Node({self.data!r})"
+        return f"Node({self.data['name']!r} by {self.data['artist']!r})"
 
 
 class LinkedList:
     def __init__(self) -> None:
         self.start: Optional[Node] = None
+        self.end: Optional[Node] = None
 
     def __iter__(self) -> Iterator[Node]:
         node = self.start
@@ -27,82 +33,62 @@ class LinkedList:
     def __repr__(self) -> str:
         parts: List[str] = ["START"]
         for node in self:
-            parts.append(repr(node.data))
+            parts.append(f"[{node.data['name']}]")
         parts.append("NIL")
-        return "\n" + " --> ".join(parts)
+        return " <-> ".join(parts)
 
-    def traverse(self) -> None:
-        for node in self:
-            print(node.data)
-
-    def insert_at_beginning(self, element: Node) -> None:
-        element.next = self.start
-        self.start = element
-
-    def insert_at_end(self, element: Node) -> None:
-        element.next = None
+    def insert_at_beginning(self, name: str, artist: str, album: str) -> None:
+        new_node = Node(name, artist, album)
         if self.start is None:
-            self.start = element
+            self.start = new_node
+            self.end = new_node
+        else:
+            new_node.next = self.start
+            self.start.prev = new_node
+            self.start = new_node
+
+    def insert_at_end(self, name: str, artist: str, album: str) -> None:
+        new_node = Node(name, artist, album)
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
             return
-        
-        tail = self.start
-        for node in self:
-            tail = node
-        tail.next = element
 
-    def insert_after_node(self, element: Node, node_reference: Any) -> bool:
-        target: Optional[Node] = None
-        for n in self:
-            if (n is node_reference) if isinstance(node_reference, Node) else (n.data == node_reference):
-                target = n
-                break
+        assert self.end is not None
+        new_node.prev = self.end
+        self.end.next = new_node
+        self.end = new_node
 
-        if target is None:
-            return False
-
-        element.next = target.next
-        target.next = element
-        return True
-
-    def delete_node(self, element_data: Any) -> bool:
+    def delete_node(self, song_name: str) -> bool:
         if self.start is None:
             return False
 
-        if self.start.data == element_data:
-            to_remove = self.start
-            self.start = self.start.next
-            to_remove.next = None
-            return True
+        for node in self:
+            if node.data["name"] == song_name:
+                if node.prev:
+                    node.prev.next = node.next
+                else:
+                    self.start = node.next
 
-        prev = self.start
-        for cur in self:
-            if cur.data == element_data:
-                prev.next = cur.next
-                cur.next = None
+                if node.next:
+                    node.next.prev = node.prev
+                else:
+                    self.end = node.prev
+                
+                node.next = None
+                node.prev = None
                 return True
-            prev = cur
-
         return False
 
-    def search(self, element_data: Any) -> Optional[Node]:
-        for n in self:
-            if n.data == element_data:
-                return n
+    def search(self, song_name: str) -> Optional[Node]:
+        for node in self:
+            if node.data["name"] == song_name:
+                return node
         return None
 
-    def to_list(self) -> List[Any]:
-        return [n.data for n in self]
-
     @classmethod
-    def from_iterable(cls, iterable: Iterable[Any]) -> LinkedList:
+    def from_iterable(cls, songs: Iterable[Dict[str, str]]) -> LinkedList:
         ll = cls()
-        tail: Optional[Node] = None
-        for item in iterable:
-            node = Node(item)
-            if ll.start is None:
-                ll.start = node
-            else:
-                assert tail is not None
-                tail.next = node
-            tail = node
+        for song in songs:
+            ll.insert_at_end(song["name"], song["artist"], song["album"])
         return ll
